9572039ecb7ae11256233814993e0699509443b3
Pragyna Naik <pragynanaik@Pragynas-MacBook-Pro.local>
2021-04-14 01:24:22 -0700

    adding project

---
+++ Hello/Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Pragyna Naik on 4/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Go Seahawks!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello/Shared/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Shared
+//
+//  Created by Pragyna Naik on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
